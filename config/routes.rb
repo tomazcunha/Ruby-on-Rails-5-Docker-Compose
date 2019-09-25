d3df4b5f9ea783131a04631a0f646b2e00e85e70
tomazcunha <tomazmcn@gmail.com>
2019-09-25 06:56:25 -0300

    Configurando nova gem, Criando helpers, Alterando nome de campos, Alterando página welcome, Configrando rotas de welcome.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  resources :coins
+
+  root to: "welcome#index"
+  get 'welcome/index' #, to: 'welcome#index' # nao precisa por o to: para nao repetir welcome#index
+  get '/inicio', to: 'welcome#index'
+
+  resources :coins    # resources mapeia as 7 rotas padrão
+  # get '/coins', to: 'coins#index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
