9acfd6da2244a65d49dec60a54d8c3254dd114a4
tomazcunha <tomazmcn@gmail.com>
2019-10-02 06:00:43 -0300

    Novo Modelo de Tipo de Mineração

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :mining_types
   root to: "welcome#index"
   get 'welcome/index' #, to: 'welcome#index' # nao precisa por o to: para nao repetir welcome#index
   get '/inicio', to: 'welcome#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_20_014406) do
+ActiveRecord::Schema.define(version: 2019_10_02_082440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2019_09_20_014406) do
     t.string "description"
     t.string "acronym"
     t.string "url_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mining_types", force: :cascade do |t|
+    t.string "name"
+    t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
