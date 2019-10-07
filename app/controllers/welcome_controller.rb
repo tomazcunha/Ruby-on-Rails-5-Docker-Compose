5c5e0b7b0908cfa69b8c61c36b7288083662f16b
tomazcunha <tomazmcn@gmail.com>
2019-10-07 17:18:48 -0300

    Criando variáveis de cookie e session e exibindo na view coins/index.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    # armazenando este valor quando a view index for chamada
+    cookies[:curso_coockie] = "Curso de Ruby on Rails [COOCKIE]"
+    session[:curso_session] = "Curso de Ruby on Rails [SESSION]"
+
     @nome = params[:nome]
     @curso = params[:curso]
   end
