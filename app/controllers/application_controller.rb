d0668d1482bb791be740d7568771d1d16b2bf8cd
tomazcunha <tomazmcn@gmail.com>
2019-10-07 17:54:35 -0300

    Criando o método set_locale no controle principal da aplicação e incluindo links para alteração do idioma.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  # ApplicationController vai ser executado antes de qualquer controle
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale]
+      cookies[:locale] = params[:locale]
+    end
+
+    if cookies[:locale]
+      if I18n.locale != cookies[:locale]
+        I18n.locale = cookies[:locale]
+      end
+    end
+    # I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
